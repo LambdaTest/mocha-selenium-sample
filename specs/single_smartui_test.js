0c17e3add375a1216c031a4cfb05ac3f94d5a896
shreyanshc <shreyanshc@lambdatest.com>
2025-06-27 14:26:26 +0530

    Added smartui sample

---
+++ specs/single_smartui_test.js
@@ -0,0 +1,52 @@
+var assert = require("assert"),
+    webdriver = require("selenium-webdriver"),
+    conf_file = process.argv[3] || "conf/single.conf.js";
+
+var caps = require("../" + conf_file).capabilities;
+
+var buildDriver = function (caps) {
+    return new webdriver.Builder()
+        .usingServer(
+            "http://" +
+            LT_USERNAME +
+            ":" +
+            LT_ACCESS_KEY +
+            "@hub.lambdatest.com/wd/hub"
+        )
+        .withCapabilities(caps)
+        .build();
+};
+
+describe("Mocha Todo Test " + caps.browserName, function () {
+    var driver;
+    this.timeout(0);
+
+    beforeEach(function (done) {
+        caps.name = this.currentTest.title;
+        driver = buildDriver(caps);
+        done();
+    });
+
+    it("can find search results", function (done) {
+        driver.get("https://lambdatest.github.io/sample-todo-app/").then(function () {
+            driver.findElement(webdriver.By.name('li1')).click().then(function () {
+                console.log("Successfully clicked first list item.");
+            });
+
+            driver.findElement(webdriver.By.name('li2')).click().then(function () {
+                console.log("Successfully clicked second list item.");
+            });
+
+            driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n').then(function () {
+                driver.findElement(webdriver.By.id('addbutton')).click().then(function () {
+                    console.log("Successfully added a new task.");
+                })
+            });
+
+            // Hook to take SmartUI snapshot
+            driver.executeScript(`smartui.takeScreenshot=mocha-selenium`);
+
+            driver.quit();
+        });
+    });
+});
